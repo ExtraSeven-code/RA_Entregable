4a031e68cee6d9cccc53edd1bbb6cd9f66a4794e
ExtraSeven-code <apexexe124@gmail.com>
2025-04-24 20:22:46 -0500

    funciona
    funciona

--- Entregable_RA/Assets/Scripts/Spawner_Torreta.cs
+++ Entregable_RA/Assets/Scripts/Spawner_Torreta.cs
@@ -10,6 +10,7 @@ public class Spawner_Torreta : MonoBehaviour
     [SerializeField] private GameObject Torreta;
     [SerializeField] private ARRaycastManager raycastManager;
     [SerializeField] private GameObject spawn_Torretas;
+    [SerializeField] private float checkRadius;
 
     private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
@@ -22,16 +23,24 @@ public class Spawner_Torreta : MonoBehaviour
             if (raycastManager.Raycast(screenPosition, hits, TrackableType.Planes))
             {
                 Pose hitPose = hits[0].pose;
-
+                Collider[] colliders = Physics.OverlapSphere(hitPose.position, checkRadius);
+                foreach (var col in colliders)
+                {
+                    if (col.CompareTag("Torreta")) // Asegúrate de asignar este tag a tu prefab de torreta
+                    {
+                        Debug.Log("Ya hay una torreta en esta posición.");
+                        return;
+                    }
+                }
 
                 GameObject newPlayer = Instantiate(Torreta, hitPose.position, hitPose.rotation);
 
-                Collider col = newPlayer.GetComponent<Collider>();
+                Collider playerCollider = newPlayer.GetComponent<Collider>();
                 spawn_Torretas.SetActive(false);
-                if (col != null)
+                if (playerCollider != null)
                 {
                     Vector3 pos = newPlayer.transform.position;
-                    pos.y += col.bounds.extents.y;
+                    pos.y += playerCollider.bounds.extents.y;
                     newPlayer.transform.position = pos;
                 }
 
